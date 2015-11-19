dcea6ea5b7917ee44609995779afa958a4d246a4
Nikhil Srivastava <niksrc@gmail.com>
2015-11-19 22:43:08 +0530

    Adds auth and user presistence

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../lib/db');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Verify user. */
+router.post('/verify', function(req, res, next) {
+  db.get(req.body.user, function(err, users){
+    if(err)
+      throw err;
+
+    if(users.length !== 1 ){
+      res.send(401);
+    }else{
+      req.session.team = users.team;
+      req.session.id = users._id;
+      req.session.save();
+      res.send({error:false});
+    }
+
+  });
+});
+
+/*Create a new user*/
+router.post('/', function(req, res, next) {
+  db.insert(req.body.user, function(err, user){
+    if(err)
+      throw err;
+    res.send(user);
+  });
 });
 
 module.exports = router;
